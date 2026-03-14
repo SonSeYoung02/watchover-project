71dee85fabeebabb44beebf4bd6f715f0c475b2f
손세영 <187326608+SonSeYoung02@users.noreply.github.com>
2026-03-15 01:03:21 +0900

    chore: 스프링부트 의존성 라이브러리 추가 및 계층 폴더 추가
    - 스프릥부트 의존성 추가
    - controller, service, repository, dto, entity 폴더 추가
    - watchover 리포지토리 생성 (해당 리포지토리는 백엔드 전용으로 사용할 예정)
    - gitinore 파일 생성 및 내용 추가

---
+++ src/main/java/com/chh/watchover/WatchoverApplication.java
@@ -0,0 +1,13 @@
+package com.chh.watchover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchoverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchoverApplication.class, args);
+	}
+
+}
