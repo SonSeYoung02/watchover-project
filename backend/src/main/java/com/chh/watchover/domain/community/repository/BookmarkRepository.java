1c1b631734f722f8f5f3f1efb42f48ec3c2f7ace
손세영 <187326608+SonSeYoung02@users.noreply.github.com>
2026-04-28 12:41:56 +0900

    feat: 북마크 이슈 수정
    - 북마크 삭제된 게시물이 삭제되지 않고 북마크에 잔존하는 오류 해결
    - 백엔드 로직 수정

--- backend/src/main/java/com/chh/watchover/domain/community/repository/BookmarkRepository.java
+++ backend/src/main/java/com/chh/watchover/domain/community/repository/BookmarkRepository.java
@@ -4,6 +4,9 @@ import com.chh.watchover.domain.community.model.entity.BookmarkEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -12,5 +15,7 @@ public interface BookmarkRepository extends JpaRepository<BookmarkEntity, Long> 
     Optional<BookmarkEntity> findByUser_userIdAndPost_postId(Long userId, Long postId);
     Page<BookmarkEntity> findByUser_UserId(Long userId, Pageable pageable);
 
-    void deleteByPost_PostId(Long postId);
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("delete from BookmarkEntity b where b.post.postId = :postId")
+    void deleteByPost_PostId(@Param("postId") Long postId);
 }
