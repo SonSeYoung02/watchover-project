85ee7031dbf56a56ae14ce5739c1d54401e813b6
손세영 <187326608+SonSeYoung02@users.noreply.github.com>
2026-04-28 13:43:31 +0900

    feat: 백엔드 수정
    - dto 변경

--- backend/src/main/java/com/chh/watchover/domain/user/model/dto/SearchResponseDto.java
+++ backend/src/main/java/com/chh/watchover/domain/user/model/dto/SearchResponseDto.java
@@ -4,15 +4,21 @@ import com.chh.watchover.domain.user.model.entity.UserEntity;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import io.swagger.v3.oas.annotations.media.Schema;
 
-
 @Schema(description = "유저 조회 응답")
-@JsonPropertyOrder({"nickname", "characterImage"})
+@JsonPropertyOrder({"nickname", "loginId", "email", "characterImage"})
 public record SearchResponseDto(
         @Schema(description = "닉네임") String nickname,
+        @Schema(description = "로그인 ID") String loginId,
+        @Schema(description = "이메일") String email,
         @Schema(description = "캐릭터 이미지 URL") String characterImage
 ) {
 
     public static SearchResponseDto from(UserEntity user, String characterImage) {
-        return new SearchResponseDto(user.getNickname(), characterImage);
+        return new SearchResponseDto(
+                user.getNickname(),
+                user.getLoginId(),
+                user.getEmail(),
+                characterImage
+        );
     }
 }
